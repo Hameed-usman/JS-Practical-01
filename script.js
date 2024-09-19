a309ed4724c2a309c39ece0d87cc97e83dd25f44
Hameed-usman <usman.hameed1145@gmail.com>
2024-09-19 13:51:02 +0500

    Practise 02

--- script.js
+++ script.js
@@ -1,19 +1,66 @@
 // console.log("Helo..!!");
 
+// PROGRAM 01
+                // "Checking Age Eligibility (AND)"
+// let age = +prompt("Enter your age:");
 
-let marks = +prompt("Enter your marks::");
+// if (age >= 18 && age <= 65) {
+//     console.log("You are eligible.");
+// } else {
+//     console.log("You are not eligible.");
+// }
+
+// PROGRAM 02  
+            //   "// Login Access (OR)" 
+
+// let username = prompt("Enter your username:");
+// let password = prompt("Enter your password:");
+
+// if (username === "admin" || password === "1234") {
+//     console.log("Access granted.");
+// } else {
+//     console.log("Access denied.");
+// }
 
 
-if (marks < 0 || marks > 100) {
-    alert("Invalid input. Please enter marks between 0 and 100.");
-} else if (marks >= 90) {
-    alert("Your grade is A");
-} else if (marks >= 80) {
-    alert("Your grade is B");
-} else if (marks >= 70) {
-    alert("Your grade is C");
-} else if (marks >= 60) {
-    alert("Your grade is D");
-} else {
-    alert("Your grade is F");
-}
+// PROGRAM 03 
+            // "Checking for Night Time (NOT)"
+
+// let hour = prompt("Enter the hour (0-23):");
+
+            
+//     if (!(hour >= 6 && hour < 18)) {
+//         console.log("It's night time.");
+//     } else {
+//             console.log("It's day time.");
+//     }
+
+
+// PROGRAM 03 
+
+        //    "Checking for Even or Odd Number"
+
+// let number = +prompt("Enter a number:");
+
+// if (number % 2 === 0) {
+//     console.log("The number is even.");
+// } else {
+//     console.log("The number is odd.");
+// }
+
+          
+// PROGRAM  04 
+          
+        //    " Checking for Weekend or Weekday" 
+
+// let day = +prompt("Enter the day of the week (1 for Sunday, 7 for Saturday):");
+
+// if (day === 1 || day === 7) {
+//     console.log("It's a weekend.");
+// } else if (day >= 2 && day <= 6) {
+//     console.log("It's a weekday.");
+// } else {
+//     console.log("Invalid input. Please enter a number between 1 and 7.");
+// }
+
+
